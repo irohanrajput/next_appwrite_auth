7d2955c97c340970470f95667b1264f1b5ceab85
irohanrajput <itusharsingh27@gmail.com>
2025-03-13 16:04:27 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='bg-red p-8 rounded-lg shadow-lg'>
+        <p>this is the login page</p>
+    </div>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen m-4 p-4'>
+      <div className='text-center'> ths is the sign up page</div>
+    </div>
+  )
+}
+
+export default page
